5771ad0c56c7289f4c3a595d142134fbf4f098bb
Boris Joffe <boris@joff3.com>
2013-06-08 10:51:31 -0700

    got name listing working for familywatchdog

--- scrape.py
+++ scrape.py
@@ -3,20 +3,24 @@
 import urllib, urllib2
 import os
 
-familywatchdog_url = "http://www.familywatchdog.us/ShowMap_Status.asp"
-#familywatchdog_url = "http://www.familywatchdog.us/ShowList.asp"
+familywatchdog_url = "http://www.familywatchdog.us/ShowMap_Status.asp" # for map view
+familywatchdog_url = "http://www.familywatchdog.us/ShowList.asp" # for map list view (not working)
+familywatchdog_url = "http://www.familywatchdog.us/ShowNameList.asp" # for name list
 
+# following are for map list
 street = '2502 North Ave'
 city = 'Odell'
 statecode = 'NE'
 zipcode = '68415'
 
+# following are for name list
+lastname = "Smith"
+firstname = "John"
+statecode = 'CA'
 
-payload = { 'txtZipCode': zipcode, 
-			'txtWidth': '',
-			'txtStreet1': street,
-			'txtState': statecode,
-			'txtBrowser': 'Y'}
+payload = { 'txtLastName': lastname, 
+			'txtFirstName': firstname,
+			'txtState': statecode,}
 payload = urllib.urlencode(payload)
 
 response = urllib2.urlopen(url=familywatchdog_url, data=payload)
