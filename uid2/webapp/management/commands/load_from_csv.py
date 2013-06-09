1191148686bf5e58333064b4049eda13a8684b48
Boris Joffe <boris@joff3.com>
2013-06-09 11:14:44 -0700

    change sex offender flag on csv import

--- uid2/webapp/management/commands/load_from_csv.py
+++ uid2/webapp/management/commands/load_from_csv.py
@@ -5,11 +5,14 @@ from django.conf import settings
 from subprocess import call
 from django.core.management.base import CommandError, NoArgsCommand
 from django.db import models
+from django.db import transaction
+from django.utils.timezone import utc
 from dateutil.relativedelta import relativedelta
 import csv
 from subprocess import call
 
 class Command(NoArgsCommand):
+	#@transaction.commit_manually
     help = 'load in data from csv'
     def handle_noargs(self, **options):
         # load in each of the rows of csv data and populate the database.
@@ -24,34 +27,37 @@ class Command(NoArgsCommand):
 		sex_offender_complaint = Field.objects.get(name='sex_offender_compliant')
 
 		i = 0
+		DBG = True
+		DBG = False
+		SPARSE_IMPORT = True	# import every 200th element
 
 		for line in reader:
-			# import every 200th element
 			i += 1
-			if i % 200 != 0:
+			if i % 200 and SPARSE_IMPORT != 0:
 				continue
 
 			print "Processing data", line
 			profile = Profile()
-			profile.expiry_date = datetime.utcnow() + relativedelta(years=+1)	
+			profile.expiry_date = datetime.utcnow().replace(tzinfo=utc) + relativedelta(years=+1)	
 			#profile.creation_date = datetime.utcnow()
 			profile.save()
 			
-			# for reference
-			# dataformat = ['Name','Address','City', 'St', 'Zip', 'County', 'Offense', 'Count', 'Compliant']
+			# dataformat = ['Name','Address','City', 'St', 'Zip', 'County', 'Offense', 'Count', 'Compliant'] 	# for reference
 
 			fv = FieldValue()
 			fv.field = last_name
 			fv.value = line[0].split(',')[1] 
 			fv.profile = profile
 			fv.save()
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
 			
 			fv = FieldValue()
 			fv.field = first_name
 			fv.value = line[0].split(',')[0].rstrip(', ')
 			fv.profile = profile
 			fv.save()
-
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
+			
 			fv = FieldValue()
 			fv.field = address
 			address_value = ''
@@ -60,24 +66,36 @@ class Command(NoArgsCommand):
 			fv.value = address_value
 			fv.profile = profile
 			fv.save()
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
 
 			fv = FieldValue()
 			fv.field = sex_offense
 			fv.value = line[6]
 			fv.profile = profile
 			fv.save()
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
 
 			fv = FieldValue()
 			fv.field = sex_offense_count
 			fv.value = line[7]
 			fv.profile = profile
 			fv.save()
-
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
+			
 			fv = FieldValue()
 			fv.field = sex_offender_complaint
 			fv.value = line[8].strip(', ')
 			fv.profile = profile
 			fv.save()
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
 
+			fv = FieldValue()
+			fv.field = sex_offender
+			fv.value = True
+			fv.profile = profile
+			fv.save()
+			if DBG: print "Saving " + str(fv.field), " = '" + fv.value + "'"
+			
 			profile.save()
+			#transaction.commit
 
