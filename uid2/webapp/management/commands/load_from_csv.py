223210e9db1ec2480943c17dec5e438e297ea354
Boris Joffe <boris@joff3.com>
2013-06-08 23:41:35 -0700

    got every 200th element from the missouri sex offender file

--- uid2/webapp/management/commands/load_from_csv.py
+++ uid2/webapp/management/commands/load_from_csv.py
@@ -6,8 +6,8 @@ from subprocess import call
 from django.core.management.base import CommandError, NoArgsCommand
 from django.db import models
 from dateutil.relativedelta import relativedelta
-from time import sleep
 import csv
+from subprocess import call
 
 class Command(NoArgsCommand):
     help = 'load in data from csv'
@@ -16,7 +16,21 @@ class Command(NoArgsCommand):
 		fp = open('msor.csv', 'rb')
 		reader = csv.reader(fp)
 
+		last_name = Field.objects.get(name='last_name')
+		first_name = Field.objects.get(name='first_name')
+		address = Field.objects.get(name='address')
+		sex_offense = Field.objects.get(name='sex_offense')
+		sex_offense_count = Field.objects.get(name='sex_offense_count')
+		sex_offender_complaint = Field.objects.get(name='sex_offender_compliant')
+
+		i = 0
+
 		for line in reader:
+			# import every 200th element
+			i += 1
+			if i % 200 != 0:
+				continue
+
 			print "Processing data", line
 			profile = Profile()
 			profile.expiry_date = datetime.utcnow() + relativedelta(years=+1)	
@@ -27,41 +41,40 @@ class Command(NoArgsCommand):
 			# dataformat = ['Name','Address','City', 'St', 'Zip', 'County', 'Offense', 'Count', 'Compliant']
 
 			fv = FieldValue()
-			fv.field = Field.objects.get(name='last_name')
-			fv.value = line[0].split(',')[1] 		# parse last name
-			print fv.value
+			fv.field = last_name
+			fv.value = line[0].split(',')[1] 
 			fv.profile = profile
-			fv.save()	# profile_id cannot be null bug
+			fv.save()
 			
 			fv = FieldValue()
-			fv.field = Field.objects.get(name='first_name')
+			fv.field = first_name
 			fv.value = line[0].split(',')[0].rstrip(', ')
 			fv.profile = profile
 			fv.save()
 
 			fv = FieldValue()
-			fv.field = Field.objects.get(name='address')
-			address = ''
+			fv.field = address
+			address_value = ''
 			for i in range(1, 6):
-				address += line[i].strip() + " "
-			fv.value = address
+				address_value += line[i] + " "
+			fv.value = address_value
 			fv.profile = profile
 			fv.save()
 
 			fv = FieldValue()
-			fv.field = Field.objects.get(name='sex_offense')
-			fv.value = line[6].strip(', ')
+			fv.field = sex_offense
+			fv.value = line[6]
 			fv.profile = profile
 			fv.save()
 
 			fv = FieldValue()
-			fv.field = Field.objects.get(name='sex_offense_count')
-			fv.value = line[7].strip(', ')
+			fv.field = sex_offense_count
+			fv.value = line[7]
 			fv.profile = profile
 			fv.save()
 
 			fv = FieldValue()
-			fv.field = Field.objects.get(name='sex_offender_compliant')
+			fv.field = sex_offender_complaint
 			fv.value = line[8].strip(', ')
 			fv.profile = profile
 			fv.save()
