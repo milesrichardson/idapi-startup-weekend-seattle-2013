7fe90a8f020b0c641de611a1f5e0db63a13dac39
adrian <theadriangreen@gmail.com>
2013-06-09 00:49:16 -0700

    remove print

--- uid2/uid2/settings.py
+++ uid2/uid2/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ROOT + '/../webapp/static/'
-print 'STATIC_ROOT:' + STATIC_ROOT
+
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
