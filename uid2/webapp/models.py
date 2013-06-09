8aba2d251146fe0b7454e521643ca21488029bf4
Miles Richardson <miles.richardson@redfin.com>
2013-06-08 18:19:42 -0700

    Models and fixtures setup

--- uid2/webapp/models.py
+++ uid2/webapp/models.py
@@ -3,24 +3,28 @@ from django.db import models
 # Create your models here.
 
 
-class ProfileModel(Model.Model):
-	#expiry_date
-	#source
-	pass
+class Profile(models.Model):
+	expiry_date = models.DateTimeField()
 
-class FieldModel(Model.Model):
-	name = models.TextField(max_length=255)
+	def __str__(self):
+		return str(self.pk)
 
-class FieldValueModel(Model.Model):
-	field = models.ForeignKey('FieldModel')
-	profile = models.ForeignKey('ProfileModel')
+class Field(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+class FieldValue(models.Model):
+	field = models.ForeignKey('Field')
+	profile = models.ForeignKey('Profile')
 	value = models.TextField()
 
 
-first_name = "Miles"
-last_name = "Richardson"
-ssn = '628-26-2612'
-profile = FieldValueModel.Objects.all(field=field.Objects.get(name='first_name'), value=first_name)\
-			.Objects.all(field=field.Objects.get(name='last_name'), value=last_name)\
-			.Objects.all(field=field.Objects.get(name='ssn', value=ssn))\
-			.profile
+# first_name = "Miles"
+# last_name = "Richardson"
+# ssn = '628-26-2612'
+# profile = FieldValueModel.Objects.all(field=field.Objects.get(name='first_name'), value=first_name)\
+# 			.Objects.all(field=field.Objects.get(name='last_name'), value=last_name)\
+# 			.Objects.all(field=field.Objects.get(name='ssn', value=ssn))\
+# 			.profile
